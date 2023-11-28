506dd5c4b8d8894a5cfa746f999badcc7aff6010
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-11-28 14:40:56 +0200

    Size attenuation

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,5 +1,5 @@
- void main()
-       {
-        gl_FragColor = vec4(1.0, 1.0, 1.0,1.0);
-        #include <colorspace_fragment>
-       }
+  void main()
+            {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+                #include <colorspace_fragment>
+            }
