5b4f0a22569bed1582d7aec4b911b7d56ab8f1a9
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-12-27 12:51:58 +0200

    fix the randomness

--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -1,6 +1,7 @@
 uniform float uSize;
 uniform float uTime;
 
+attribute vec3 aRandomness;
 attribute float aScale;
 
 varying vec3 vColor;
@@ -13,13 +14,16 @@ void main()
              */
             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-            //Spin
+            // Spin
             float angle = atan(modelPosition.x, modelPosition.z);
             float distanceToCenter = length(modelPosition.xz);
             float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
             angle += angleOffset;
             modelPosition.x = cos(angle)* distanceToCenter;
             modelPosition.z = sin(angle)* distanceToCenter;
+
+           // Randomness
+           modelPosition.xyz += aRandomness;
 
             vec4 viewPosition = viewMatrix * modelPosition;
             vec4 projectedPosition = projectionMatrix * viewPosition;
