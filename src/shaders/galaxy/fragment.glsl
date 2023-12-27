887dd05e6a2c4747b1854ded7e3856431f548954
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-12-27 11:10:29 +0200

    light point pattern

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,15 +1,20 @@
   void main()
-            {
-            //    // Disc
-            //    float strength = distance(gl_PointCoord, vec2(0.5));
-            //    strength = step(0.5, strength);
-            //    strength = 1.0 - strength;
+        {
+            // // Disc
+            //  float strength = distance(gl_PointCoord, vec2(0.5));
+            //  strength = step(0.5, strength);
+            //  strength = 1.0 - strength;
 
-               // Diffuse Point
-               float strength = distance(gl_PointCoord,vec2(0.5));
-               strength *= 2.0;
-               strength = 1.0 - strength;
+            // // Diffuse Point
+            //  float strength = distance(gl_PointCoord,vec2(0.5));
+            //  strength *= 2.0;
+            //  strength = 1.0 - strength;
 
-                gl_FragColor = vec4(vec3(strength), 1.0);
+            // Light Point
+            float strength = distance(gl_PointCoord,vec2(0.5));
+            strength = 1.0 - strength;
+            strength = pow(strength, 10.0);
+
+            gl_FragColor = vec4(vec3(strength), 1.0);
                 #include <colorspace_fragment>
-            }
+         }
